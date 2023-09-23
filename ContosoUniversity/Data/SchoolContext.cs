0aa9073a1f9b8d6630e016a23015aec8cab089d7
Siim Koppel <siimk999@gmail.com>
2023-09-23 21:34:00 +0300

    Fix null reference warnings

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -9,9 +9,9 @@ namespace ContosoUniversity.Data
         {
         }
 
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> ?Courses { get; set; }
+        public DbSet<Enrollment> ?Enrollments { get; set; }
+        public DbSet<Student> ?Students { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
