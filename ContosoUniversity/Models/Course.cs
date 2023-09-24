f583629609880a3742c57f25e06575b4af752ed9
Siim Koppel <siimk999@gmail.com>
2023-09-24 13:01:23 +0300

    Add Department navigation property to Course

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -14,9 +14,11 @@ namespace ContosoUniversity.Models
         [Range(0, 5)]
         public int Credits { get; set; }
         public int DepartmentID { get; set; }
-       // public Department Department { get; set; }
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+
        // public ICollection<CourseAssignment> CourseAssignments { get; set; }
-       // TODO add navigation properties
+       
+        // TODO add navigation properties
     }
 }
