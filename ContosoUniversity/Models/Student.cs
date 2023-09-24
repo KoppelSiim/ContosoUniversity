2acf19d6c603d297981d703f801dabe5cc3df1dd
Siim Koppel <siimk999@gmail.com>
2023-09-24 11:27:29 +0300

    Add Required and Display attributes to Student model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -6,12 +6,16 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
         [StringLength(50)]
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Display(Name = "Last Name")]
         public string? LastName { get; set; }
+        [Required]
         [StringLength(50)]
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string? FirstMidName { get; set; }
 
         // The DataType attribute is used to specify a data type that's more specific than the database intrinsic type.
@@ -19,8 +23,22 @@ namespace ContosoUniversity.Models
         [DataType(DataType.Date)]
         // The DisplayFormat attribute is used to explicitly specify the date format.
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        // FullName is a calculated property that returns a value that's created by concatenating two other properties.
+        // Therefore it has only a get accessor, and no FullName column will be generated in the database.
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        // The Display attribute specifies that the caption for the text boxes should be
+        // "First Name", "Last Name", "Full Name", and "Enrollment Date" instead of the
+        // property name in each instance (which has no space dividing the words).
 
         public ICollection<Enrollment>? Enrollments { get; set; }
     }
