b7407dddbd08840d3f230359368b1228ab2576d0
Siim Koppel <siimk999@gmail.com>
2023-09-24 14:23:28 +0300

    Add Migration ComplexDataModel

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -15,7 +15,6 @@ namespace ContosoUniversity.Models
         public int StudentID { get; set; }
         [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
-
         public Course Course { get; set; }
         public Student Student { get; set; }
     }
--- ContosoUniversity/Models/OfficeAssignment.cs
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -20,7 +20,6 @@ namespace ContosoUniversity.Models
         [StringLength(50)]
         [Display(Name = "Office Location")]
         public string? Location { get; set; }
-
         public Instructor Instructor { get; set; }
     }
 }
