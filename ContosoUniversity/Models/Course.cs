9f3f6db7f66f30f4f4511396d23d3b6db179c6b5
Siim Koppel <siimk999@gmail.com>
2023-09-24 12:36:18 +0300

    Add DepartmentID foreign key property to Course

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -12,7 +12,10 @@ namespace ContosoUniversity.Models
         public string? Title { get; set; }
         [Range(0, 5)]
         public int Credits { get; set; }
-
+        public int DepartmentID { get; set; }
+       // public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+       // public ICollection<CourseAssignment> CourseAssignments { get; set; }
+       // TODO add navigation properties
     }
 }
