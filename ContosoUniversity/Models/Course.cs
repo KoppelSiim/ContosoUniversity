a82531e829e13206254ece5a4f8529e216248457
Siim Koppel <siimk999@gmail.com>
2023-09-24 12:24:11 +0300

    Add validation and display to Course

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -6,8 +6,11 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
-        public string Title { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string? Title { get; set; }
+        [Range(0, 5)]
         public int Credits { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
